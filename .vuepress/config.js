51bbd01a6debaef44c39683595e9b86abbcaa133
elbernat <elbernat@gmail.com>
2018-05-29 08:19:41 -0400

    KCJ CT OFFLINE GAMES SELECTION
    KCJ CT OFFLINE GAMES SELECTION

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    title: 'Personal Freedom',
-    locales: {
-        './': {lang: 'en-US'},
-        // './fr/': {lang: 'fr'}
-    },
+    title: 'KCJ OFFLINE GAMES',
+    description: 'A curated selection of RTP offlines games',
+    // locales: {
+    //     './': {lang: 'en-US'},
+    //     './fr/': {lang: 'fr'}
+    // },
     themeConfig: {
         nav: [
             {text: 'Page-1', link: 'PAGE-1.md'}
+        ],
+        sidebar :[
+            './',
+            './PAGE-1'
         ]
-        // sidebar :[
-        //     './',
-        //     './PAGE-1'
-        // ]
     }
 }
